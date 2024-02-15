8955b18c5ed51fac9db01741a25a35190c159c06
Dominique Haeusler <dominiquehaeusler@gmail.com>
2024-02-15 12:19:43 +0100

    play around with colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,8 @@
 require('rose-pine').setup({
 	dark_variant = 'moon',
-	--disable_background = true
+	disable_background = false,
+	dim_inactive_windows = false,
+	extend_background_behind_borders = true,
 })
 
 vim.cmd.colorscheme "rose-pine"
